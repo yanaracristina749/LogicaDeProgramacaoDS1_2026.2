d48ab061d9e4866d2459a93ec0e584f8fe4d67ce
yanaracristina1 <yanaracristina749@gmail.com>
2026-09-16 13:12:15 -0300

    resposta exercicio 01 gestao_trafego

--- 02_conceitos_basicos/exercicio_01_gestao_trafego.py
+++ 02_conceitos_basicos/exercicio_01_gestao_trafego.py
@@ -12,3 +12,8 @@ Calcule e mostre na tela o Custo Por Clique (CPC) médio da campanha formatado e
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+
+campanha = input("qual foi o valor investido na campanha?")
+cliques = input ("qual o número total de cliques?")
+resposta = float(campanha)/int(cliques)
+print(f"{resposta:.2f} R$ por cliques")
