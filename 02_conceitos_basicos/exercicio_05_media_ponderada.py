b3f661e0d0a5c742fb3fde490760794b78b337a6
yanaracristina1 <yanaracristina749@gmail.com>
2026-09-17 15:55:57 -0300

    exerc.05_media

--- 02_conceitos_basicos/exercicio_05_media_ponderada.py
+++ 02_conceitos_basicos/exercicio_05_media_ponderada.py
@@ -9,3 +9,9 @@ Calcule e exiba a média final ponderada utilizando apenas operadores aritmétic
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
+nota1 = float(input ("qual a sua primeira nota da avaliação?"))
+nota2 = float(input ("qual a sua segunda nota da avaliação?"))
+nota3 = float(input ("qual a sua terceita nota da avaliação?"))
+resultado1 = (nota1*2)+(nota2*3)+(nota3*5)
+resultado2 = resultado1/10
+print(f"a média é :{resultado2:.2f}")
