dc3cbd347e2ef377142a5363eb359004ce806bfd
yanaracristina1 <yanaracristina749@gmail.com>
2026-09-17 16:04:53 -0300

    desafio_01

--- 02_conceitos_basicos/desafio_01_bug_matematico.py
+++ 02_conceitos_basicos/desafio_01_bug_matematico.py
@@ -20,3 +20,8 @@ SUA MISSÃO:
 # print("A média do aluno é:", media)
 
 # TODO: Escreva aqui o código corrigido:
+# CÓDIGO ORIGINAL COM BUG (Analise e corrija):
+nota1 = float (input("Digite a primeira nota: "))
+nota2 = float (input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+print(f"A média do aluno é:{media:.2f}")
