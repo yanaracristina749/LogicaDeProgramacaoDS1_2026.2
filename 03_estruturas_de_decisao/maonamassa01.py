88675577bdca7c7e64e88e00045fb356d66b0af9
yanaracristina1 <yanaracristina749@gmail.com>
2026-09-23 12:52:48 -0300

    maonamassa.py0301

---
+++ 03_estruturas_de_decisao/maonamassa01.py
@@ -0,0 +1,4 @@
+media_aluno = float(input ("qual a media do aluno?"))
+frequencia_percentual = int(input ("qual a frequencia percentual do aluno?"))
+resultado = (media_aluno)>=6 and (frequencia_percentual)>=75
+print("Status de aprovação:",resultado)
