401d02604278ca975acc41e06e278b9ba33f4996
yanaracristina1 <yanaracristina749@gmail.com>
2026-09-24 13:57:56 -0300

    maonamassa03.py

---
+++ 03_estruturas_de_decisao/maonamassa03.py
@@ -0,0 +1,10 @@
+opcao = int(input("Digite a opção desejada (1, 2 ou 3): "))
+if opcao == 1: 
+    print("Consultar livro")
+elif opcao == 2: 
+    print("Ralizar empréstimo")
+elif opcao == 3:
+    print ("Devolver livro")
+else:
+    print("Opçãõ não enontrada")
+        
